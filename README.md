5662582997b5b3ad95cef1b84e2e334bbb46e09e
jefferson-p <JeffersonP@byui.edu>
2023-11-30 19:54:09 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Overview
+The software written is a simple webscrape of the most viewed wikipedia pages in a certain year. The program scrapes the data from the site, and inserts it into a pandas datatable. The purpose of writing this software is to learn how to webscrape complex websites and provide the groundwork for data science projects in the future. 
+
+
+
+[Software Demo Video](https://youtu.be/BN1wh_1kfhc)
+
+# Development Environment
+
+VSCode
+Python
+Pandas
+Numpy
+Selenium
+
+# Useful Websites
+
+{Make a list of websites that you found helpful in this project}
+
+- [Chat GPT](https://chat.openai.com/)
+- [GeeksForGeeks](https://www.geeksforgeeks.org/selenium-python-tutorial/)
+- [Topviews](https://pageviews.wmcloud.org/topviews/?project=en.wikipedia.org&platform=all-access&date=2020&excludes=)
+
+# Future Work
+- Add user imput functionality
+- Create a machine learning model to apply the dataset to
+- Add functionality to further scrape each link in the dataset
---
+++ project.py
@@ -0,0 +1,65 @@
+# %%
+import time
+import pandas as pd
+import numpy as np
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+# %%
+year = 2020
+url = f"https://pageviews.wmcloud.org/topviews/?project=en.wikipedia.org&platform=all-access&date={year}&excludes="
+driver = webdriver.Edge()
+driver.get(url)
+# %%
+time.sleep(10) 
+# %%
+# Specify the number of entries to retrieve
+num_entries = 5
+# %%
+# Get the base XPath for the entries
+base_xpath = '//*[@id="topview-entry-'
+# %%
+entry_list = []
+# %%
+# Loop through the range of numbers
+for entry_number in range(2, 25):
+    # Construct the full XPath for the current entry
+    entry_xpath = f'{base_xpath}{entry_number}"]'
+
+    if not driver.find_elements(By.XPATH, entry_xpath):
+        continue
+
+    # Find the entry element
+    entry_info = driver.find_element(By.XPATH, entry_xpath)
+    # Extract information for the current entry
+    list_data = entry_info.text.split('\n')
+    numbers = list_data[2].split()
+    list_data.pop(2)
+    list_data.extend(numbers)
+
+    # Get the date information
+    date_info = driver.find_element(By.ID, 'date-input')
+    date_data = date_info.get_attribute('value')
+
+    # Get the label href
+    label_href = entry_info.find_element(By.XPATH, './/td[2]/div/a').get_attribute('href')
+
+    # Create a dictionary with the extracted information
+    entry_dict = {
+        "Year": date_data,
+        "Rank": list_data[0],
+        "Label": list_data[1],
+        "Edits": list_data[2],
+        "Editors": list_data[3],
+        "Views": list_data[4],
+        "Mobile Percentage": list_data[5],
+        "Link": label_href
+    }
+    
+    entry_list.append(entry_dict)
+
+    time.sleep(3)
+# Append the dictionary to the DataFrame
+df = pd.DataFrame(entry_list)
+
+df
+# %%
